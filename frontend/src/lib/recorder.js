f6be19a81d4781af4fce8693ffc3bc7c12f13ce7
Markus Miertschink <markus.miertschink@solarwinds.com>
2020-04-15 16:33:43 +0200

    setup recorder

--- frontend/src/lib/recorder.js
+++ frontend/src/lib/recorder.js
@@ -2,15 +2,16 @@
 const {
   BatchRecorder,
   jsonEncoder: { JSON_V2 }
-} = require('zipkin');
-const { HttpLogger } = require('zipkin-transport-http');
+} = require("zipkin");
+const { HttpLogger } = require("zipkin-transport-http");
 
-const debug = 'undefined' !== typeof window
-  ? window.location.search.indexOf('debug') !== -1
-  : process.env.DEBUG;
+const debug =
+  "undefined" !== typeof window
+    ? window.location.search.indexOf("debug") !== -1
+    : process.env.DEBUG;
 
 // Send spans to Zipkin asynchronously over HTTP
-const zipkinBaseUrl = ZIPKIN ? ZIPKIN : 'http://zipkin:9411';
+const zipkinBaseUrl = ZIPKIN ? ZIPKIN : "http://zipkin:9411";
 
 const httpLogger = new HttpLogger({
   endpoint: `${zipkinBaseUrl}/api/v2/spans`,
@@ -18,13 +19,15 @@ const httpLogger = new HttpLogger({
 });
 
 function recorder(serviceName) {
-  return debug ? debugRecorder(serviceName) : new BatchRecorder({ logger: httpLogger });
+  return debug
+    ? debugRecorder(serviceName)
+    : new BatchRecorder({ logger: httpLogger });
 }
 
 function debugRecorder(serviceName) {
   // This is a hack that lets you see the data sent to Zipkin!
   const logger = {
-    logSpan: (span) => {
+    logSpan: span => {
       const json = JSON_V2.encode(span);
       console.log(`${serviceName} reporting: ${json}`);
       httpLogger.logSpan(span);
@@ -34,13 +37,15 @@ function debugRecorder(serviceName) {
   const batchRecorder = new BatchRecorder({ logger });
 
   // This is a hack that lets you see which annotations become which spans
-  return ({
-    record: (rec) => {
+  return {
+    record: rec => {
       const { spanId, traceId } = rec.traceId;
-      console.log(`${serviceName} recording: ${traceId}/${spanId} ${rec.annotation.toString()}`);
+      console.log(
+        `${serviceName} recording: ${traceId}/${spanId} ${rec.annotation.toString()}`
+      );
       batchRecorder.record(rec);
     }
-  });
+  };
 }
 
 module.exports.recorder = recorder;
