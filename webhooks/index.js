0bd3dad9cab27bc55eedc8635b18b3d6c345e85d
Markus Miertschink <markus.miertschink@solarwinds.com>
2020-05-18 10:09:49 +0200

    modified webhook code

--- webhooks/index.js
+++ webhooks/index.js
@@ -1,26 +1,51 @@
-var express = require("express");
-var app = express();
-var childProcess = require("child_process");
+import childProcess from "child_process";
+import crypto from "crypto";
+import http from "http";
 var githubUsername = "obiwan007";
 
-app.get("/webhooks/github", function (req, res) {
-  console.log("Received GET ");
-});
+const SECRET = "THEDAMNSECRET";
+// app.get("/webhooks/github", function (req, res) {
+//   console.log("Received GET ");
+// });
 
-app.post("/webhooks/github", function (req, res) {
-  console.log("Request", req);
-  var sender = req.body.sender;
-  var branch = req.body.ref;
+// app.post("/webhooks/github", function (req, res) {
+//   console.log("Request", req);
+//   var sender = req.body.sender;
+//   var branch = req.body.ref;
 
-  if (branch.indexOf("master") > -1 && sender.login === githubUsername) {
-    deploy(res);
-  }
-});
+//   if (branch.indexOf("master") > -1 && sender.login === githubUsername) {
+//     deploy(res);
+//   }
+// });
+
+http
+  .createServer((req, res) => {
+    req.on("data", (chunk) => {
+      const signature = `sha1=${crypto
+        .createHmac("sha1", SECRET)
+        .update(chunk)
+        .digest("hex")}`;
+
+      const isAllowed = req.headers["x-hub-signature"] === signature;
+
+      const body = JSON.parse(chunk);
+
+      const isMaster = body.ref === "refs/heads/master";
+
+      if (isAllowed && isMaster) {
+        // do something
+        deploy(res);
+      }
+    });
+
+    res.end();
+  })
+  .listen(8080);
 
 function deploy(res) {
   console.log("Receved trigger");
   childProcess.exec(
-    "cd /home/markus/dev/usersrv && make docker && make kreapply",
+    "cd /home/markus/dev/usersrv && git pull && make docker && make kreapply",
     function (err, stdout, stderr) {
       if (err) {
         console.error(err);
@@ -30,7 +55,3 @@ function deploy(res) {
     }
   );
 }
-
-app.listen(38080, "0.0.0.0", function () {
-  console.log("Example app listening on port 38080!");
-});
