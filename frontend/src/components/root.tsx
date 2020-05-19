99c9611020389c5336746b057e000cbd441ad9ea
Markus Miertschink <markus.miertschink@solarwinds.com>
2020-05-19 12:26:03 +0200

    logging removed

--- frontend/src/components/root.tsx
+++ frontend/src/components/root.tsx
@@ -185,7 +185,6 @@ export function MainMenu(props: any) {
                 }
               }
             });
-            console.log("Currenct", currentTimer && currentTimer!.isRunning, currentTimer )
             const timerText =
               currentTimer && currentTimer!.isRunning && !activeRoute("/timer", location)
                 ? showElapsed(currentTimer)
